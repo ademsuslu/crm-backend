f98a89f279c772f55a3470fd5a56684573997fa9
ademsuslu <ademsuslu9080@gmail.com>
2024-11-15 00:28:56 +0300

    refactor:reminder routes

--- routes/reminderRoutes.js
+++ routes/reminderRoutes.js
@@ -2,12 +2,10 @@ const express = require('express')
 const router = express.Router()
 const reminderController = require('../controllers/reminderController')
 
-router.get('/', reminderController.getAllReminder)
 router.post('/', reminderController.createReminder)
-// router.get('/search', reminderController.searchCustomer)
-// router.get('/', reminderController.getAllReminder)
-// router.get('/:id', reminderController.getCustomerById)
-// router.put('/:id', reminderController.updateCustomer)
-// router.delete('/:id', reminderController.deleteCustomer)
+router.get('/', reminderController.getAllReminder)
+router.get('/:id', reminderController.getReminderById)
+router.put('/:id', reminderController.updateReminder)
+router.delete('/:id', reminderController.deleteReminder)
 
 module.exports = router
