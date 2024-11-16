172ce185a0a63c84a25f96292d91e028b35efba6
ademsuslu <ademsuslu9080@gmail.com>
2024-11-16 16:58:49 +0300

    refactor:relete search routes getAllOpportunityDetails

--- routes/opportunityRoutes.js
+++ routes/opportunityRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router()
 const opportunityController = require('../controllers/opportunityController')
 
 router.post('/', opportunityController.createOpportunity)
-// router.get('/search', opportunityController.searchCustomer)
 router.get('/', opportunityController.getAllOpportunity)
 router.get('/:id', opportunityController.getOpportunityById)
 router.put('/:id', opportunityController.updateOpportunity)
