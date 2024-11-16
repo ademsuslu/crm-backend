4dc5682cd3ae0b3899fcf8d2b9043dffe9ae3102
ademsuslu <ademsuslu9080@gmail.com>
2024-11-16 17:07:33 +0300

    refactor:opportunıty conrtoller

--- controllers/opportunityController.js
+++ controllers/opportunityController.js
@@ -17,8 +17,8 @@ exports.createOpportunity = async (req, res) => {
 // Tüm müşterileri listeleme
 exports.getAllOpportunity = async (req, res) => {
   try {
-    const opportunity = await Opportunity.find()
-    res.status(200).json(opportunity)
+    const opportunities = await Opportunity.find().populate('assignedTo')
+    res.status(200).json(opportunities)
   } catch (error) {
     res.status(500).json({ message: error.message })
   }
@@ -27,10 +27,11 @@ exports.getAllOpportunity = async (req, res) => {
 // Belirli bir müşteri bilgisi getirme
 exports.getOpportunityById = async (req, res) => {
   try {
-    const opportunity = await Opportunity.findById(req.params.id)
-    if (!opportunity) {
-      return res.status(404).json({ message: 'Opportunity not found' })
-    }
+    const opportunity = await Opportunity.findById(req.params.id).populate(
+      'assignedTo'
+    )
+    if (!opportunity)
+      return res.status(404).json({ message: 'Fırsat bulunamadı' })
     res.status(200).json(opportunity)
   } catch (error) {
     res.status(500).json({ message: error.message })
@@ -45,17 +46,14 @@ exports.updateOpportunity = async (req, res) => {
       req.body,
       {
         new: true,
+        runValidators: true,
       }
     )
-    if (!opportunity) {
-      return res.status(404).json({ message: 'Opportunity not found' })
-    }
-    res.status(200).json({
-      data: opportunity,
-      message: 'Edit Success',
-    })
+    if (!opportunity)
+      return res.status(404).json({ message: 'Fırsat bulunamadı' })
+    res.status(200).json(opportunity)
   } catch (error) {
-    res.status(400).json({ message: 'Edit Unsuccess!' })
+    res.status(400).json({ message: error.message })
   }
 }
 
@@ -63,10 +61,9 @@ exports.updateOpportunity = async (req, res) => {
 exports.deleteOpportunity = async (req, res) => {
   try {
     const opportunity = await Opportunity.findByIdAndDelete(req.params.id)
-    if (!opportunity) {
-      return res.status(404).json({ message: 'Opportunity not found' })
-    }
-    res.status(200).json({ message: 'Opportunity has been deleted' })
+    if (!opportunity)
+      return res.status(404).json({ message: 'Fırsat bulunamadı' })
+    res.status(200).json({ message: 'Fırsat başarıyla silindi' })
   } catch (error) {
     res.status(500).json({ message: error.message })
   }
