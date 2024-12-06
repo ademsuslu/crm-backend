0f51a322c04c7767a8c997f28dafbd5239e68e54
ademsuslu <ademsuslu9080@gmail.com>
2024-12-06 18:16:17 +0300

    refactor:blog routes

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,13 +1,13 @@
 const express = require('express')
-
 const blogController = require('../controllers/blogController')
 
 const router = express.Router()
 
-router.get('/', blogController.getAllBlog)
-router.post('/', blogController.createBlog)
-router.get('/:id', blogController.getBlogById)
-router.get('/:title', blogController.getBlogByTitle)
-router.patch('/:id', blogController.updateBlog)
-router.delete('/:id', blogController.deleteBlog)
+router.get('/', blogController.getAllBlog) // Tüm blogları getir
+router.post('/', blogController.createBlog) // Yeni blog oluştur
+router.get('/title/:title', blogController.getBlogByTitle) // Başlığa göre getir
+router.get('/:id', blogController.getBlogById) // ID'ye göre getir
+router.patch('/:id', blogController.updateBlog) // Blog güncelle
+router.delete('/:id', blogController.deleteBlog) // Blog sil
+
 module.exports = router
